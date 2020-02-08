b3b3569816751d462c77c58c20a458206c0878a8
Max Marlatt <m.marlatt@setonhill.edu>
2020-02-07 20:00:12 -0500

    Initial Commit

---
+++ Mad libs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mad libs
+//
+//  Created by Maxwell Marlatt on 2/7/20.
+//  Copyright © 2020 Maxwell Marlatt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
