b1fca1848164ca963215682c246ce654ff133270
Max Marlatt <m.marlatt@setonhill.edu>
2020-03-19 11:42:35 -0400

    Had trouble with this one for some reason

--- Mad libs/AppDelegate.swift
+++ Mad libs/AppDelegate.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+mclass AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
--- Mad libs/ViewController.swift
+++ Mad libs/ViewController.swift
@@ -9,10 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var nameField : UITextField = "My name is <name>"
+    @IBOutlet weak var nounTextField: UITextField!
+   @IBOutlet weak var verbTextField: UITextField!
+   @IBOutlet weak var adjectiveTextField: UITextField!
+   @IBOutlet weak var adjective2TextField: UITextField!
+    @IBOutlet weak var noun2TextField: UITextField!
+   @IBOutlet weak var outputTextView: UITextField!
+    
+     @IBAction func enterButton(sender: AnyObject) {
+        var inputText: String
+        outputTextView = UITextField(inputText)
+    nameField.replacingOccurrences(of:"<name>",with: adjectiveTextField)
+    }
+        
+      
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
